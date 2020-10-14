f256673a8a471098b4c9dea3abec1fe708c56af5
김범중 <gimbeomjung@gimbeomjung-ui-MacBookAir.local>
2020-10-14 21:14:02 +0900

    Initial commit

---
+++ app/src/main/java/com/kbj/iot_team_kbj/MypageActivity.java
@@ -0,0 +1,14 @@
+package com.kbj.iot_team_kbj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MypageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mypage);
+    }
+}
